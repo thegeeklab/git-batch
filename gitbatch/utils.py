26db928d2a29ea58834841558c0d2a81dbb14c10
Robert Kaussow <mail@thegeeklab.de>
2023-10-16 11:44:02 +0200

    fix: remove deprecated distutils

--- gitbatch/utils.py
+++ gitbatch/utils.py
@@ -2,7 +2,6 @@
 """Global utility methods and classes."""
 
 import os
-from distutils.util import strtobool
 
 
 def normalize_path(path):
@@ -10,6 +9,30 @@ def normalize_path(path):
         return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
 
     return None
+
+
+def strtobool(value):
+    """Convert a string representation of truth to true or false."""
+
+    _map = {
+        "y": True,
+        "yes": True,
+        "t": True,
+        "true": True,
+        "on": True,
+        "1": True,
+        "n": False,
+        "no": False,
+        "f": False,
+        "false": False,
+        "off": False,
+        "0": False
+    }
+
+    try:
+        return _map[str(value).lower()]
+    except KeyError as err:
+        raise ValueError(f'"{value}" is not a valid bool value') from err
 
 
 def to_bool(string):
